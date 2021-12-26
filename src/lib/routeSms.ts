f60c5a26b20c5ad411e111c808d9211b6a8f43d1
RazakAlpha <36008239+RazakAlpha@users.noreply.github.com>
2021-12-26 13:10:38 +0000

    Added static members to improve usage, flexibility

--- src/lib/routeSms.ts
+++ src/lib/routeSms.ts
@@ -2,59 +2,69 @@ import axios from "axios";
 import { IConfig, IMessageResponse, IParam } from "../interfaces/param";
 
 export class routeSms {
-    private params_:IParam = {protocol:'http', port: 8080, }
-    private config_: IConfig
+    private static params_:IParam = {protocol:'http', port: 8080, }
+    private static config_: IConfig
 
-    private errorCodes: Map<number, string> = new Map()
+    private static errorCodes: Map<number, string> = new Map()
 
-    constructor(param: IParam){
+    constructor(param?: IParam){
 
-        this.params_ = param;
-        if (!this.params_.path){
-            this.params_.path = '/bulksms/bulksms'
+        if(param){
+            routeSms.params_ = param;
+            if (! routeSms.params_.path){
+                routeSms.params_.path = '/bulksms/bulksms'
+            }
+    
+            if(! routeSms.params_.host){
+                throw new Error("Host is not defined");
+                
+            }
         }
 
-        if(!this.params_.host){
-            throw new Error("Host is not defined");
-            
-        }
 
         // set default config
-        this.config_ = {
+        routeSms.config_ = {
             type: 0,
             dlr:0,
             source: 'sender',
         }
 
         // set the error codes
-        this.errorCodes.set(1701, 'Success, Message Submitted Successfully')
-        this.errorCodes.set(1702, 'Invalid URL');
-        this.errorCodes.set(1703, 'Invalid Username or password');
-        this.errorCodes.set(1704, 'Invalid type field');
-        this.errorCodes.set(1705, 'Invalid message');
-        this.errorCodes.set(1706, 'Invalid destination');
-        this.errorCodes.set(1707, 'Invalid source or sender');
-        this.errorCodes.set(1708, 'Invalid value for dlr field');
-        this.errorCodes.set(1709, 'User validation field');
-        this.errorCodes.set(1710, 'Internal Error');
-        this.errorCodes.set(1715, 'Response timeout');
-        this.errorCodes.set(1725, 'Insufficient Credit');
-        this.errorCodes.set(1726, 'Insufficient reseller Credit');
+        routeSms.errorCodes.set(1701, 'Success, Message Submitted Successfully')
+        routeSms.errorCodes.set(1702, 'Invalid URL');
+        routeSms.errorCodes.set(1703, 'Invalid Username or password');
+        routeSms.errorCodes.set(1704, 'Invalid type field');
+        routeSms.errorCodes.set(1705, 'Invalid message');
+        routeSms.errorCodes.set(1706, 'Invalid destination');
+        routeSms.errorCodes.set(1707, 'Invalid source or sender');
+        routeSms.errorCodes.set(1708, 'Invalid value for dlr field');
+        routeSms.errorCodes.set(1709, 'User validation field');
+        routeSms.errorCodes.set(1710, 'Internal Error');
+        routeSms.errorCodes.set(1715, 'Response timeout');
+        routeSms.errorCodes.set(1725, 'Insufficient Credit');
+        routeSms.errorCodes.set(1726, 'Insufficient reseller Credit');
+
+
+        return this;
     }
 
     
-    public set config(v : IConfig) {
-        this.config_ = v;
+    public static set config(v : IConfig) {
+        routeSms.config_ = v;
+    }
+
+    public static get engine(){
+        return routeSms;
     }
 
 
-    async sendAsync(params: { Content: string, From: string, To: number[]| number, config?: IConfig}) {
+    static async sendAsync(params: { Content: string, From: string, To: number[]| number, config?: IConfig}) {
 
         if(params.config){
-            this.config = {...params.config}
+            routeSms.config_ = {...params.config}
         }
 
-        this.config_.source = params.From;
+        routeSms.config_.source = params.From;
 
         let contacts: string = ''
         if(Array.isArray(params.To) && params.To.length > 0){
@@ -69,9 +79,9 @@ export class routeSms {
         }
 
         try{
-           return  axios.post(`${this.params_.protocol}://${this.params_.host}${this.params_.path}?username=${this.params_.username}&password=${this.params_.password}&type=${this.config_.type}&dlr=${this.config_.dlr}&destination=${contacts}&source=${this.config_.source}&message=${params.Content}&url=${this.config_.url}`)
+           return  axios.post(`${routeSms.params_.protocol}://${routeSms.params_.host}${routeSms.params_.path}?username=${routeSms.params_.username}&password=${routeSms.params_.password}&type=${routeSms.config_.type}&dlr=${routeSms.config_.dlr}&destination=${contacts}&source=${routeSms.config_.source}&message=${params.Content}&url=${routeSms.config_.url}`)
            .then(results => {
-               return this.convertResponse(results.data)
+               return routeSms.convertResponse(results.data)
            }).catch(err => {
                console.log(err)
            })
@@ -81,13 +91,13 @@ export class routeSms {
         
     }
 
-    sendSync(params: { Content: string, From: string, To: number[]| number, config?: IConfig}) {
+    static sendSync(params: { Content: string, From: string, To: number[]| number, config?: IConfig}) {
 
         if(params.config){
-            this.config = {...params.config}
+            this.config_ = {...params.config}
         }
 
-        this.config_.source = params.From;
+        routeSms.config_.source = params.From;
 
         let contacts: string = ''
         if(Array.isArray(params.To) && params.To.length > 0){
@@ -102,9 +112,9 @@ export class routeSms {
         }
 
     
-        return axios.post(`${this.params_.protocol}://${this.params_.host}${this.params_.path}?username=${this.params_.username}&password=${this.params_.password}&type=${this.config_.type}&dlr=${this.config_.dlr}&destination=${contacts}&source=${this.config_.source}&message=${params.Content}&url=${this.config_.url}`)
+        return axios.post(`${routeSms.params_.protocol}://${routeSms.params_.host}${routeSms.params_.path}?username=${routeSms.params_.username}&password=${routeSms.params_.password}&type=${routeSms.config_.type}&dlr=${routeSms.config_.dlr}&destination=${contacts}&source=${routeSms.config_.source}&message=${params.Content}&url=${routeSms.config_.url}`)
         .then((response) => { 
-            const  res = this.convertResponse(response.data);
+            const  res = routeSms.convertResponse(response.data);
             // console.log(response.data, res)
             return res;
         }).catch(function(err){
@@ -116,13 +126,13 @@ export class routeSms {
     }
 
 
-    convertResponse(value: string) {
+    static convertResponse(value: string) {
         const responses = value.split(',');
         const converted: IMessageResponse[] =[];
 
         return responses.map(response => {
             const parts = response.split('|');
-            return {status: parts[0] =='1701' ? 'successful': 'failed', code:parts[0],message: this.errorCodes.get(Number(parts[0])), destination: parts[1], id: parts[2] }
+            return {status: parts[0] =='1701' ? 'successful': 'failed', code:parts[0],message: routeSms.errorCodes.get(Number(parts[0])), destination: parts[1], id: parts[2] }
         })
     }
     
